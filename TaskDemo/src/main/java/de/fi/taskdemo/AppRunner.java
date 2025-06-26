4ba279d5d116c1b7f7bae3d8c7f6cfcdcc7ddb8f
Jo Wagner <jowagner@limago.de>
2025-06-26 11:41:18 +0200

    initial commit

---
+++ TaskDemo/src/main/java/de/fi/taskdemo/AppRunner.java
@@ -0,0 +1,43 @@
+package de.fi.taskdemo;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class AppRunner implements CommandLineRunner {
+
+
+    
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    public AppRunner(final JobLauncher jobLauncher, final Job job) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        final int anzahlSteps = 10;
+        //boolean b = false;
+
+        System.out.println( "\nJoblauf mit Job-Parameter anzahlSteps=" + anzahlSteps + ":" );
+        JobExecution je = jobLauncher.run( job,
+                new JobParametersBuilder()
+
+                        .addString(
+                            TaskletJobConfiguration.ANZAHLSTEPS_KEY, "" + anzahlSteps )
+                        .toJobParameters() );
+        for( StepExecution se : je.getStepExecutions() ) {
+            System.out.println("StepExecution " + se.getId() + ": StepName = " + se.getStepName() +
+                    ", CommitCount = " + se.getCommitCount());
+        }
+    }
+}
