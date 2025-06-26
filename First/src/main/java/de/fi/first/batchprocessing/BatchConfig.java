6c6889670396c9250bbac5c7e51f329c2a5ebc5c
Jo Wagner <jowagner@limago.de>
2025-06-26 11:05:34 +0200

    initial commit

--- First/src/main/java/de/fi/first/batchprocessing/BatchConfig.java
+++ First/src/main/java/de/fi/first/batchprocessing/BatchConfig.java
@@ -1,16 +1,21 @@
 package de.fi.first.batchprocessing;
 
 import de.fi.first.entity.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.skip.SkipLimitExceededException;
+import org.springframework.batch.core.step.skip.SkipPolicy;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileParseException;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +24,9 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Configuration
 public class BatchConfig {
@@ -57,18 +65,65 @@ public class BatchConfig {
                       PlatformTransactionManager transactionManager,
                       FlatFileItemReader<Person> reader,
                       PersonItemProcessor processor,
-                      JdbcBatchItemWriter<Person> writer) {
+                      JdbcBatchItemWriter<Person> writer, SkipPolicy skipPolicy) {
         return new StepBuilder("step1", jobRepository)
                 .<Person, Person>chunk(10, transactionManager)
                 .reader(reader)
+                .faultTolerant()
+                //.skipLimit(2)
+                //.skip(FlatFileParseException.class)
+                //.noSkip(FileNotFoundException.class)
+                .skipPolicy(skipPolicy)
                 .processor(processor)
                 .writer(writer)
                 .build();
     }
     @Bean
-    public Job importUserJob(JobRepository jobRepository, Step step1) {
+    public Job importUserJob(JobRepository jobRepository,JobCompletionNotificationListener listener, Step step1) {
         return new JobBuilder("importUserJob", jobRepository)
+                .listener(listener)
         .start(step1).build();
+    }
+
+    @Bean
+    SkipPolicy createSkipPolicy() {
+
+        return new SkipPolicy() {
+
+            private final Logger logger = LoggerFactory.getLogger("badRecordLogger");
+
+            @Override
+            public boolean shouldSkip(final Throwable exception, final long skipCount) throws SkipLimitExceededException {
+
+                if (exception instanceof FileNotFoundException) {
+                    return false;
+                } else if (exception instanceof FlatFileParseException && skipCount <= 2) {
+                    FlatFileParseException ffpe = (FlatFileParseException) exception;
+
+                    StringBuilder errorMessage = new StringBuilder();
+                    errorMessage.append("An error occured while processing the ");
+                    errorMessage.append(ffpe.getLineNumber());
+                    errorMessage.append(" line of the file '");
+
+                    Pattern pattern = Pattern.compile(".*(\\W\\w+\\.csv).*");
+                    Matcher matcher = pattern.matcher(ffpe.toString());
+                    if (matcher.matches())
+                        errorMessage.append(matcher.group(1));
+                    else
+                        errorMessage.append("unknown");
+                    errorMessage.append("'. Below was the faulty input.");
+                    errorMessage.append("\n");
+                    errorMessage.append(ffpe.getInput());
+                    errorMessage.append("\n");
+                    logger.error("{}", errorMessage.toString());
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+
+        };
     }
 
 }
